59020ca00ff4db7b73f3c18b6c29eb4df15031ef
sozi <sozyarnadir@gmail.com>
2022-10-24 19:11:13 +0300

    1

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { actors } from "../data";
 
+
+
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+    const actorsList = actors.map((actor) => ( <
+        div key = { actor.name } >
+        <
+        h3 > { actor.name } < /h3> <
+        ul > {
+            actor.movies.map((movie) => ( <
+                li key = { movie } > { movie } < /li>
+            ))
+        } <
+        /ul> <
+        /div>
+    ))
+
+
+    return ( <
+        div >
+        <
+        h1 > Actors page < /h1> { actorsList } <
+        /div>
+    );
 }
 
 export default Actors;
