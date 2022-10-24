f22f349fc1fbb80bbd43c8433a5f9a3829073231
sozi <sozyarnadir@gmail.com>
2022-10-24 19:15:59 +0300

    6

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+    return ( <
+        div className = "navbar" >
+        <
+        ul >
+
+        <
+        li > < NavLink to = '/Home' > Home < /NavLink> </li >
+        <
+        li > < NavLink to = '/Movies' > Movies < /NavLink> </li >
+        <
+        li > < NavLink to = '/Actors' > Actors < /NavLink> </li >
+        <
+        li > < NavLink to = '/Directors' > Directors < /NavLink> </li >
+
+        <
+        /ul> < /
+        div >
+    );
 }
 
 export default NavBar;
