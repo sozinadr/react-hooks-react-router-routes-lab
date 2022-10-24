56547be421d4f127c7c996a6009de48b0086e3e5
sozi <sozyarnadir@gmail.com>
2022-10-24 19:13:56 +0300

    3

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,26 @@
-import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+    const directorsList = directors.map((director) => ( <
+        div key = { director.name } >
+        <
+        h3 > { director.name } < /h3> <
+        ul > {
+            director.movies.map((e) => ( <
+                li key = { e } > { e } < /li>
+            ))
+        } <
+        /ul>
+
+        <
+        /div>
+    ))
+
+
+    return <div >
+        <
+        h1 > Directors Page < /h1>  { directorsList } <
+        /div>;
 }
 
 export default Directors;
