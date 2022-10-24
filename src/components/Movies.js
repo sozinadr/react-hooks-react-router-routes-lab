cb2a1f433017941f3202ae47e23b462110c3cbe0
sozi <sozyarnadir@gmail.com>
2022-10-24 19:15:18 +0300

    5

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,37 @@
 import React from "react";
 import { movies } from "../data";
 
+
+
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+    const moviesList = movies.map((movie) => ( <
+        div key = { movie.title } >
+        <
+        h3 > { movie.title } < /h3>  <
+        h5 > RunTime: { movie.time }
+        m < /h5> <
+        ul >
+
+        {
+            movie.genres.map((genre) => ( <
+                li key = { genre } > { genre } < /li>
+            ))
+        }
+
+        <
+        /ul> <
+        /div>
+    ))
+
+
+    return ( <
+        div >
+        <
+        h1 > Movies Page < /h1> { moviesList } <
+        /div> 
+    );
 }
 
 export default Movies;
